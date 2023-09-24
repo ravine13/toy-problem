9d9056714d800b00650e168175d2bf24a0500a1c
ravine13 <kuriaderek@gmail.com>
2023-09-24 20:55:42 +0300

    initial commit

---
+++ grade1.js
@@ -0,0 +1,29 @@
+function assignGrade(marks) {
+    if(isNaN(marks) || marks < 0 || marks > 100){
+    }
+  
+    let grade;
+  
+    if (marks > 79) {
+      grade = "A";
+    } else if (marks >= 60 && marks <= 79) {
+      grade = "B";
+    } else if (marks >= 50 && marks <= 59) {
+      grade = "C-";
+    } else if (marks >= 40 && marks <= 49) {
+      grade = "D-";
+    } else {
+      grade = "F";
+    }
+  
+    return grade;
+  }
+  
+  const userInput = prompt("Please enter your marks (0-100):");
+  const marks = parseFloat(userInput);
+  const grade = calculateGrade(marks);
+  
+  if (grade !== undefined) {
+    alert(`Your grade is: ${grade}`);
+  }
+  assignGrade();
---
+++ speed.js
@@ -0,0 +1,26 @@
+function calculateDemeritPoints(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+  
+    if (speed <= speedLimit) {
+      console.log("Ok");
+    } else {
+      const demeritPoints = Math.floor((speed - speedLimit) / kmPerPoint);
+      
+      if (demeritPoints > 12) {
+        console.log("License suspended");
+      } else {
+        console.log(`Points: ${demeritPoints}`);
+      }
+    }
+  }
+  
+  const userInput = prompt("Enter the car's speed (in km/h):");
+  const speed = parseFloat(userInput);
+  
+  if (!isNaN(speed)) {
+    calculateDemeritPoints(speed);
+  } else {
+    console.log("Invalid input. Please enter a valid number.");
+  }
+  
